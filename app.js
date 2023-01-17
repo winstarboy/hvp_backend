01fcae5835f1abd31ebf58f555a91dc844c064ed
Kewin Sebaasti <19uca001@loyolacollege.edu>
2023-01-17 16:32:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ app.use(routes);
 app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
 app.use(express.json({ limit: "50mb", extended: true, parameterLimit: 50000 }))
 
-const PORT = process.env.PORT || 12707;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 12707;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+app.listen(12707, '0.0.0.0');
